1792b86ca7149aa3c47ad08c6f3ac6b68a363d99
RaquelAlvarezz <raquel.alvarez@vigo.org>
2022-01-24 10:46:25 +0100

    changues

---
+++ dist/login_basico.php
@@ -0,0 +1,7 @@
+<?php
+echo "Nombre introducido: ". $_POST['nombre']. "<br>";
+echo "Apellido introducido: ". $_POST['apellidos']. "<br>";
+echo "Usuario introducido: ". $_POST['usuario']. "<br>";
+echo "Ciudad introducida: ". $_POST['ciudad']. "<br>";
+echo "Provincia introducida: ". $_POST['provincia']. "<br>";
+echo "Codigo introducido: ". $_POST['codigo']. "<br>";
